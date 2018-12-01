bfb772a038f0853c1b297704dce5d5abdcf06cb9
ErnestoVC <netito86@hotmail.com>
2018-11-30 21:41:43 -0500

    Add files via upload

---
+++ BOT/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    SECRET_KEY = 'my_secret_token'
+    PAGE_ACCESS_TOKEN = 'EAAEZBuYBIyl4BAPOLlIyGEh0W4s3jwqwfZB4CaZCVW9EF4sNG2VpHZAOcTCUPr7skUm6q0FuNhp7fe7ZCLLvaqUY2en0Y7MD'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
